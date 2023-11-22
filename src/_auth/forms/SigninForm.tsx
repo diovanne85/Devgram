67c87d33b16e583f3ec0da4339f6b651a31a704e
diovanne85 <dpsagosoy85@gmail.com>
2023-11-22 21:39:37 +0800

    adjust

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -103,7 +103,7 @@ const SigninForm = () => {
             )}
           </Button>
 
-          <p className="text-small-regular text-secondary-color text-center mt-2">
+          <p className="text-small-regular text-black text-center mt-2">
            I Don&apos;t have an account?
             <Link
               to="/sign-up"
