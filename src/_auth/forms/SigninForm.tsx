6e0cd471ae5b9b2a704022fed987afd746da2eda
Hirayamanawari <jonalynmanguba@gmail.com>
2023-11-22 19:22:52 +0800

    This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
    Currently, two official plugins are available:
    
    - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
    - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -56,15 +56,15 @@ const SigninForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.png" height='100' width='350' alt="logo" />
 
-        <h2 className="h3-bold md:h2-bold pt-2 sm:pt-8">
+        <h2 className="h3-bold md:h2-bold pt-2 sm:pt-8 text-primary-color">
           Log in to your account
         </h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">
-          Welcome back! Please enter your details.
+        <p className="text-secondary-color small-medium md:base-regular mt-2">
+          Start connecting to Developer community
         </p>
         <form
           onSubmit={form.handleSubmit(handleSignin)}
-          className="flex flex-col gap-5 w-full mt-4">
+          className="flex  flex-col gap-5 w-full mt-4">
           <FormField
             control={form.control}
             name="email"
@@ -103,11 +103,11 @@ const SigninForm = () => {
             )}
           </Button>
 
-          <p className="text-small-regular text-light-2 text-center mt-2">
+          <p className="text-small-regular text-secondary-color text-center mt-2">
             Don&apos;t have an account?
             <Link
               to="/sign-up"
-              className="text-primary-500 text-small-semibold ml-1">
+              className="text-secondary-color text-small-semibold ml-1">
               Sign up
             </Link>
           </p>
